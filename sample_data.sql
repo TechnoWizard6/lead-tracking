fb16fd5b1faf4e568433626b0ef737fba6db26d0
TechnoWizard6 <markkrish696@gmail.com>
2026-06-30 11:23:11 +0530

    Insert sample lead data into leads table

---
+++ sample_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO leads
+(
+lead_token,
+name,
+phone,
+page_name,
+page_url
+)
+
+VALUES
+(
+'abc123',
+'Hii',
+'whatsapp:+917057273525',
+'YOO Villas for Hii',
+'https://6a3d17e4f455624e2c44955a--relaxed-buttercream-4fb006.netlify.app'
+);
